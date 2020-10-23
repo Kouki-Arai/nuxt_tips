c1522f7544adf8ea5babd440aa3b6bac91db3331
新井公貴 <koukiarai@koukiarai.local>
2020-10-23 21:45:04 +0900

    add: header item

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1 class="header-title"><nuxt-link class="link" to="/">Nuxt.js Tips</nuxt-link></h1>
+  </header>
+</template>
+<style lang="scss" scoped>
+header {
+  padding: 20px 4% 20px;
+  width: 100%;
+  background-color: rgb(61,197,142);
+  position: fixed;
+  top: 0;
+  margin: 0 auto;
+}
+.header-title {
+  text-align: center;
+}
+.link {
+  color: rgb(53,73,93);
+  text-decoration: none;
+}
+</style>
